1021ea4677dcc1adcbc49cb11afcd048e4b538b6
GRIMMJOW-YK <yadhu1948@gmail.com>
2025-07-08 12:12:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPoster() {
+  document.getElementById('intro').classList.add('hidden');
+  document.getElementById('main').classList.remove('hidden');
+  document.getElementById('introVideo').pause();
+}
