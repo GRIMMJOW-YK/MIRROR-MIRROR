92b5e8b71886c67b4ae263829ccf869d1cb3ef43
GRIMMJOW-YK <yadhu1948@gmail.com>
2025-07-08 12:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const startBtn = document.getElementById('startBtn');
-const intro = document.getElementById('intro');
-const mainContent = document.getElementById('mainContent');
-const bgm = document.getElementById('bgm');
-
-startBtn.addEventListener('click', () => {
-  intro.style.display = 'none';
-  mainContent.style.display = 'block';
-  bgm.play();
-});
+non
